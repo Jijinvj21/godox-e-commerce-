b7109e98c06c4e60a6964422244aa6eca00ff98e
Jijin vj <jijinvjvazhippilly@gmail.com>
2023-01-15 20:33:11 +0530

    update 20:33 /15 - 01- 2023

--- app.js
+++ app.js
@@ -43,6 +43,6 @@ app.use('/',paymentRoute)
 
 
 
-app.listen(8348, function () {
-  console.log("Server is running on port 8348 ");
+app.listen(3000, function () {
+  console.log("Server is running on port 3000 ");
 });
